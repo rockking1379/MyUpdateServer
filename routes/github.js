6a5d56631a27eb16d01e796af583d39f0bb5c873
James <rockking1379@gmail.com>
2015-11-22 23:42:16 -0700

    had issues with some of the things

--- routes/github.js
+++ routes/github.js
@@ -1,13 +1,13 @@
 /**
  * Created by James on 11/22/2015.
  */
-var childProcess = require('child_process');
+var spawn = require('child_process').spawn;
 
 var express = require('express');
 var router = express.Router();
 
 router.post('/', function (req, res){
-   childProcess.exec('./hooks/post-receive');
+   spawn('sh', ['./hooks/post-receive']);
 
     res.status(200);
     res.send();
