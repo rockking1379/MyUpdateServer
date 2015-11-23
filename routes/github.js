74cd7c1febb11b1714e5a5ca9fee3d1c60a921ac
James <rockking1379@gmail.com>
2015-11-22 23:23:27 -0700

    try again
    no more descrips

--- routes/github.js
+++ routes/github.js
@@ -1,12 +1,13 @@
 /**
  * Created by James on 11/22/2015.
  */
-var githubhook = require('githubhook');
-var github = githubhook({'host':'update.codegeekhosting.me', 'port':3001});
 var childProcess = require('child_process');
 
-github.on('push', function(repo, ref, data){
-    childProcess.exec('./hooks/post-receive');
-    });
+var express = require('express');
+var router = express.Router();
 
-github.listen();
+router.post('/', function (req, res){
+   childProcess.exec('./hooks/post-receive');
+});
+
+module.exports = router;
